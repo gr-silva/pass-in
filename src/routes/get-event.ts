b8dc14f48cb18023a86e611da9c2935ef26cfba6
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-04-03 18:59:57 -0300

    feat: add event response types

--- src/routes/get-event.ts
+++ src/routes/get-event.ts
@@ -11,12 +11,35 @@ export async function getEvent(app: FastifyInstance) {
         params: z.object({
           eventId: z.string().uuid(),
         }),
-        response: {},
+        response: {
+          200: {
+            event: z.object({
+              id: z.string().uuid(),
+              title: z.string(),
+              details: z.string().nullable(),
+              slug: z.string(),
+              maximumAttendees: z.number().int().nullable(),
+              attendeesAmount: z.number().int(),
+            }),
+          },
+        },
       },
     },
     async (request, reply) => {
       const { eventId } = request.params;
       const event = await prisma.event.findUnique({
+        select: {
+          id: true,
+          title: true,
+          details: true,
+          slug: true,
+          maximumAttendees: true,
+          _count: {
+            select: {
+              attendees: true,
+            },
+          },
+        },
         where: {
           id: eventId,
         },
@@ -24,7 +47,16 @@ export async function getEvent(app: FastifyInstance) {
       if (event === null) {
         throw new Error("Event not found.");
       }
-      return reply.status(200).send({ event });
+      return reply.status(200).send({
+        event: {
+          id: event.id,
+          title: event.title,
+          details: event.details,
+          slug: event.slug,
+          maximumAttendees: event.maximumAttendees,
+          attendeesAmount: event._count.attendees,
+        },
+      });
     }
   );
 }
