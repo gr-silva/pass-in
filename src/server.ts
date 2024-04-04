0c79574e119a15358f29ab1429e0a840c4a8c568
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-04-03 23:31:29 -0300

    feat: add cors origin

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
 
 import {
   serializerCompiler,
@@ -17,6 +18,10 @@ import { getEventAttendees } from "./routes/get-event-attendees";
 import { errorHandler } from "./error-handler";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 app.register(fastifySwagger, {
   swagger: {
@@ -48,6 +53,6 @@ app.register(getEventAttendees);
 
 app.setErrorHandler(errorHandler);
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running!");
 });
